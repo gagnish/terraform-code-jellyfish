7942408b7ef93d3d5976357adc58c0f785b4ab17
gagnish <73713601+gagnish@users.noreply.github.com>
2022-11-15 19:34:46 +0530

    Update ec2.tf
    new

--- ec2.tf
+++ ec2.tf
@@ -1,29 +1,12 @@
-#create a ec2 instance with existing vpc subnet
+
 resource "aws_instance" "dev-instance" {
-  ami           = "ami-002068ed284fb165b"
+  ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
   count         = 2
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   vpc_security_group_ids = [ aws_security_group.lb.id ]
-  key_name = "dev-server"
+  key_name = "myubuntukey"
   tags = {
-    Name = "dev-ec2-server"
-  }
-  connection {    
-       type     = "ssh"    
-       user     = "ec2-user"    
-       private_key = file("./dev-server.pem")    
-       host     = self.public_ip  
-  }
-  provisioner "remote-exec" {    
-    inline = [      
-      "sudo amazon-linux-extras enable nginx1", 
-      "sudo yum install -y nginx",
-      "sudo service nginx restart",
-      "sudo systemctl enable nginx.service", 
-      "sudo service nginx status",
-      ]  
+    Name = "dev-ec2-server" 
   }
 }
-
-#terraform destroy -target RESOURCE_TYPE.NAME
