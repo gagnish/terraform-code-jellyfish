4df4c85a5b44c9430d0cafda93c6d6aa3a5e899a
gagnish <73713601+gagnish@users.noreply.github.com>
2022-11-15 19:36:10 +0530

    Update vpc.tf
    new comit

--- vpc.tf
+++ vpc.tf
@@ -19,14 +19,3 @@ resource "aws_subnet" "public" {
     Name = "dev-public-subnet"
   }
 }
-
-resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.ec2-vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.ec2-vpc.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-  tags = {
-    Name = "dev-private-subnet"
-  }
-
-}
